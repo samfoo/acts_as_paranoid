b126e2f9e848615932c148d59ab9141d992d6fdd
Sam Gibson <sam.gibson@blist.com>
2008-09-24 16:18:46 -0700

    Added support for checking deleted on :include'd associations

--- lib/caboose/acts/paranoid.rb
+++ lib/caboose/acts/paranoid.rb
@@ -16,8 +16,8 @@ module Caboose #:nodoc:
     #   Widget.find_with_deleted(:all)
     #   # SELECT * FROM widgets
     #
-    #   Widget.find_only_deleted(:all)
-    #   # SELECT * FROM widgets WHERE widgets.deleted_at IS NOT NULL
+    #   Widget.find(:all, :with_deleted => true)
+    #   # SELECT * FROM widgets
     #
     #   Widget.find_with_deleted(1).deleted?
     #   # Returns true if the record was previously destroyed, false if not 
@@ -30,9 +30,6 @@ module Caboose #:nodoc:
     #
     #   Widget.count_with_deleted
     #   # SELECT COUNT(*) FROM widgets
-    #
-    #   Widget.count_only_deleted
-    #   # SELECT COUNT(*) FROM widgets WHERE widgets.deleted_at IS NOT NULL
     #
     #   Widget.delete_all
     #   # UPDATE widgets SET deleted_at = '2005-09-17 17:46:36'
@@ -90,41 +87,23 @@ module Caboose #:nodoc:
             end
           end
 
-          def find_only_deleted(*args)
-            options = args.extract_options!
-            validate_find_options(options)
-            set_readonly_option!(options)
-            options[:only_deleted] = true # yuck!
-
-            case args.first
-              when :first then find_initial(options)
-              when :all   then find_every(options)
-              else             find_from_ids(args, options)
-            end
-          end
-
           def exists?(*args)
-            with_deleted_scope { exists_with_deleted?(*args) }
-          end
-
-          def exists_only_deleted?(*args)
-            with_only_deleted_scope { exists_with_deleted?(*args) }
+            options = args.last.is_a?(Hash) ? args.last : {}
+            with_deleted_scope(options) { exists_with_deleted?(*args) }
           end
 
           def count_with_deleted(*args)
             calculate_with_deleted(:count, *construct_count_options_from_args(*args))
           end
 
-          def count_only_deleted(*args)
-            with_only_deleted_scope { count_with_deleted(*args) }
-          end
-
           def count(*args)
-            with_deleted_scope { count_with_deleted(*args) }
+            options = args.last.is_a?(Hash) ? args.last : {}
+            with_deleted_scope(options) { count_with_deleted(*args) }
           end
 
           def calculate(*args)
-            with_deleted_scope { calculate_with_deleted(*args) }
+            options = args.last.is_a?(Hash) ? args.last : {}
+            with_deleted_scope(options) { calculate_with_deleted(*args) }
           end
 
           def delete_all(conditions = nil)
@@ -136,12 +115,66 @@ module Caboose #:nodoc:
               default_timezone == :utc ? Time.now.utc : Time.now
             end
 
-            def with_deleted_scope(&block)
-              with_scope({:find => { :conditions => ["#{table_name}.#{deleted_attribute} IS NULL OR #{table_name}.#{deleted_attribute} > ?", current_time] } }, :merge, &block)
+            def scope_with_conditions(klass, &block)
+              deleted_condition = "%s.%s IS NULL OR %s.%s > ?"
+
+              with_scope({:find => 
+                  {:conditions => 
+                    [deleted_condition % ([klass.table_name, klass.deleted_attribute] * 2), current_time]
+                  }
+                }, :merge, &block)
             end
 
-            def with_only_deleted_scope(&block)
-              with_scope({:find => { :conditions => ["#{table_name}.#{deleted_attribute} IS NOT NULL AND #{table_name}.#{deleted_attribute} <= ?", current_time] } }, :merge, &block)
+            def scope_for_include(context, includes, &block)
+              return lambda do 
+                case includes
+                when Symbol
+                  klass = eval(context.reflect_on_association(includes).class_name)
+
+                  if !klass.paranoid?
+                    with_scope({:find => {}}, :merge, &block)
+                  else
+                    # Include the class in the conditions if it's paranoid.
+                    scope_with_conditions(klass, &block)
+                  end
+                when Array
+                  # The flying spaghetti monster touched this line of code with 
+                  # his noodley appendage.
+                  #
+                  # Breakdown:
+                  #
+                  # For each item in an includes array, recurse to this method 
+                  # and scope the query with that include passing in the 
+                  # original block as the scope block. Remember the scope
+                  # block and go to the next element in the array. Repeat the
+                  # process, but instead of passing in the original block, 
+                  # pass in the remembered scope block. Rinse and repeat.
+                  reduced = includes.inject(block) { |memo, i| scope_for_include(context, i, &memo) }
+
+                  # Wrap the final product in a scope.
+                  with_scope({:find => {}}, :merge, &reduced)
+                when Hash
+                  # This is basically the same principle as the array reduction
+                  # except that we scope both the key and the value.
+                  reduced = includes.keys.inject(block) do |memo, k| 
+                    klass = eval(context.reflect_on_association(k).class_name)
+                    scope_for_include(context, k, 
+                      &scope_for_include(klass, includes[k], &memo)
+                    )
+                  end
+
+                  # Wrap the final product in a scope.
+                  with_scope({:find => {}}, :merge, &reduced)
+                end
+              end
+            end
+
+            def with_deleted_scope(options, &block)
+              if options[:include]
+                scope_with_conditions(self, &scope_for_include(self, options[:include], &block))
+              else
+                scope_with_conditions(self, &block)
+              end
             end
 
           private
@@ -149,17 +182,19 @@ module Caboose #:nodoc:
             def find_every(options)
               options.delete(:with_deleted) ? 
                 find_every_with_deleted(options) :
-                options.delete(:only_deleted) ? 
-                  with_only_deleted_scope { find_every_with_deleted(options) } :
-                  with_deleted_scope { find_every_with_deleted(options) }
+                with_deleted_scope(options) { find_every_with_deleted(options) }
             end
         end
 
         def destroy_without_callbacks
-          unless new_record?
-            self.class.update_all self.class.send(:sanitize_sql, ["#{self.class.deleted_attribute} = ?", (self.deleted_at = self.class.send(:current_time))]), ["#{self.class.primary_key} = ?", id]
+          if respond_to?(self.class.deleted_attribute.to_s)
+            unless new_record?
+              self.class.update_all self.class.send(:sanitize_sql, ["#{self.class.deleted_attribute} = ?", self.class.send(:current_time)]), ["#{self.class.primary_key} = ?", id]
+            end
+            freeze
+          else
+            destroy_without_callbacks!
           end
-          freeze
         end
 
         def destroy_with_callbacks!
